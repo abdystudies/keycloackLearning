bd99420f05e3bf54508545c584581ad6a5196319
abdy <abdyrush@gmail.com>
2026-03-02 11:29:09 +0100

    setup iniziale di keycloak, setup di angular, creati i servizi core 'auth' e la sua guarda, creato il servizio 'spesaService' per le chiamate all'api fatta con flask, create le pagiune home, listaspesa e profile, sistemate le rotte di angular. lato backend: creato app e auth.py per l'api e l'autenticazione. sistemato il file docker-compose.yml per avere il salvataggio ogni volta che si riapre.(ESERCIZIO 1)

---
+++ frontend/src/app/services/spesa-service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import Keycloak from 'keycloak-js';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class SpesaService {
+  private http = inject(HttpClient);
+  private keycloak = inject(Keycloak);
+  //ricordate di aprire la porta del server
+  private baseUrl = 'https://3cfkbzcd-5000.euw.devtunnels.ms/'
+  //ci serve per allegare il token ad ogni
+  //richiesta http
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      Authorization: `Bearer ${this.keycloak.token}`,
+    });
+  }
+  getItems(): Observable<{ items: string[]; user: string }> {
+    return this.http.get<{ items: string[]; user: string }>(
+      `${this.baseUrl}/items`,
+      { headers: this.getHeaders() }
+    );
+  }
+  addItem(item: string): Observable<{ items: string[] }> {
+    return this.http.post<{ items: string[] }>(
+      `${this.baseUrl}/items`,
+      { item },
+      { headers: this.getHeaders() }
+    );
+  }
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    keycloak: {
+        url: 'https://3cfkbzcd-8080.euw.devtunnels.ms',
+        realm: 'prova',
+        clientId: 'provapp',
+        redirectUri: window.location.origin,
+    }
+};
